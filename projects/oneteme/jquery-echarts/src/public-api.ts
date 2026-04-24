21cbe39568c18ee987427e2552bff1b2acd464cd
Dorian <dorianpernot2@gmail.com>
2026-04-24 11:46:44 +0200

    feat(jquery-echarts): add View support + bump version to 0.0.1-beta.0
    - ChartComponent: implement OnChanges/OnDestroy, add @Input() view (ViewConfig),
      introduce _viewFacade (ChartViewFacade) and _effectiveConfig to support
      series visibility toggling via the View panel
    - chart.component.html: bind [config] to _effectiveConfig instead of config
    - Add ChartViewFacade in component/view/chart-view.facade.ts
    - public-api.ts: export ChartViewFacade
    - package.json: version 0.0.1-beta.0, peerDep echarts ^6.0.0, jquery-core ^0.0.29

--- projects/oneteme/jquery-echarts/src/public-api.ts
+++ projects/oneteme/jquery-echarts/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of @oneteme/jquery-echarts
  */
 export * from './lib/component/chart.component';
+export * from './lib/component/view/chart-view.facade';
 export * from './lib/directive/chart.directive';
 export * from './lib/directive/utils/types';
 export * from './lib/directive/utils/chart-utils';
